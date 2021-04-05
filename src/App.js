8a09bcf0d332e38fda1e8121794afbd9b0aa5f44
Savelyev Evgenyi <info.evgen.2020@gmail.com>
2021-04-05 15:52:35 +0300

    get class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Header } from './components';
+import { Home, Cart } from './Pages';
+import axios from 'axios';
+import { setPizzas } from './redux/actions/pizzas';
+// import store from './redux/store';
+import { connect } from 'react-redux';
+
+
+
+
+
+
+// function App() {
+//   const [stPizza, setPizza] = React.useState([]);
+
+// React.useEffect(() => {
+
+// axios.get('http://localhost:3000/db.json').then(({data}) => {
+//   setPizza(data.pizzas);
+// })
+// },[]);
+
+
+//   return (
+//     <div>
+//         <div className="wrapper">
+//       <Header />
+//       <div className="content">
+//       <Route path='/' render={() => <Home items={stPizza }/>} exact/>
+//         <Route path='/cart' component={Cart} exact/>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
+class App extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      this.props.setPizzas(data.pizzas);
+    })
+  }
+
+  render() {
+    return (
+
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route path='/' render={() => <Home items={this.props.items} />} exact />
+          <Route path='/cart' component={Cart} exact />
+        </div>
+      </div>
+
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items
+  }
+};
+
+const mapDispatchToProps ={
+  
+    setPizzas,
+    
+  
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps )(App);
---
+++ src/redux/actions/filters.js
@@ -0,0 +1,10 @@
+
+const setSort = (name) => ({
+   type: 'SET_SORT',
+   payload: name,
+});
+
+const setCategory = (indexCat) => ({
+   type: 'SET_CATEGORY',
+   payload: indexCat,
+});
